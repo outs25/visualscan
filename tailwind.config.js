b6cb2a80ad0be455af6611d242743be1b21254b1
outs25 <shreya.a.shelke2@gmail.com>
2026-03-04 21:56:11 +0530

    Add Tailwind CSS configuration with custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neonBlue: "#00eaff",
+        softRed: "#ff3b5c"
+      }
+    }
+  },
+  plugins: []
+}
